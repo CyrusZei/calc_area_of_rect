552b56d492a413912c0ff5fa2b8fa9ff1856888c
Cyrus Zei <cyruszei@gmail.com>
2016-02-22 11:45:38 +0100

    done

---
+++ index.js
@@ -0,0 +1,5 @@
+//index
+
+exports.calculateArea = function(a,b){
+  return a * b;
+};
---
+++ test/test.js
@@ -0,0 +1,16 @@
+//test
+
+var expect = require("chai").expect;
+var index = require('../index.js');
+
+describe('Calculate area of rectangle', function() {
+  it('should return a number', function(){
+    var output = index.calculateArea(2,5);
+    expect(output).to.be.a('number');
+  });
+  it('should return product of two numbers', function(){
+    var output = index.calculateArea(2,5);
+    expect(10).to.equal(2*5);
+  });
+
+});
